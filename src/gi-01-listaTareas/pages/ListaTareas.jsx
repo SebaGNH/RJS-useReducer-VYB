1ea6e0af69deadf0a0e5105ab232331ae167fb08
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-09-29 15:59:21 -0300

    se agregan elementos a lista

--- src/gi-01-listaTareas/pages/ListaTareas.jsx
+++ src/gi-01-listaTareas/pages/ListaTareas.jsx
@@ -2,22 +2,28 @@ import React, { useReducer, useRef } from 'react';
 import {ButtonBack} from '../../components/ButtonBack';
 
 // recibe el estado y la acción
-const reducer = (state = {}, action = {}) => {
+const reducer = (state = [], action = {}) => {
   // console.log(action); //Object { type: "add_task", title: "aaa" }
   switch (action.type) {
-    case 'add_task':
+    case 'prueba':
       return (
-        console.log(action.title)
+        console.log(action.title) // Hola <-- viene del input
+
       )
+    case 'add_task':
+      return [
+        ...state, { id: state.length, title: action.title }
+      ]
 
   }
-  return state
+  return state // este sería el default: { state }
 }
 
 export const ListaTareas = () => {
   const inputRef = useRef();
   // dispatch: evento para modificar el estado internamente
   const [state, dispatch] = useReducer(reducer, reducer());
+  // console.log('state', state);
 
   // Submit
   const handleSubmit = (ev) => {
@@ -27,6 +33,7 @@ export const ListaTareas = () => {
       type: 'add_task',// tipo de evento o acción queremos realizar
       title: inputRef.current.value
     });
+    inputRef.current.value = ''; // limpiamos input
   }
 
   return (
@@ -35,6 +42,7 @@ export const ListaTareas = () => {
       <h1>ListaTareas</h1>
       <div className="d-flex flex-row">
         <form onSubmit={handleSubmit} className='col-md-6'>
+        <h2>Formulario</h2>
           <input
             type="text"
             placeholder='Tarea'
@@ -45,9 +53,22 @@ export const ListaTareas = () => {
           <button type="submit" className='btn btn-primary mt-3'>Enviar</button>
         </form>
         <div className="col-md-6 mx-2">
-          <p>Lista</p>
+          <h3>Lista</h3>
+            <ul>
+          {
+              state?.map((lista, idx) => (
+                <li key={idx}>{lista.title}</li>
+              ))
+          }
+            </ul>
         </div>
       </div>
     </>
   )
 }
+
+
+/*
+Probar sacar el ref por una variable o value
+
+*/
