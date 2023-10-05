3a66bc7ae69fafeea92d658a8a5e48949b4d2471
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-10-04 21:38:20 -0300

    se agrega eventopara restar y reiniciar a 0

--- src/lc-01-contador/CounterLC.jsx
+++ src/lc-01-contador/CounterLC.jsx
@@ -9,6 +9,8 @@ const reducer = (state = initialState, action = {}) => {
 
   switch (action.type) {
     case 'incrementar':  return { counter: state.counter + 1 }
+    case 'decrementar':  return { counter: state.counter - 1 }
+    case 'reset':  return { counter: 0 }
 
   }
   return state
@@ -27,6 +29,14 @@ export const CounterLC = () => {
         className='btn btn-primary mx-1'
         onClick={ () => dispatch( { type: 'incrementar' } ) }
       > +1 </button>
+      <button
+        className='btn btn-primary mx-1'
+        onClick={ () => dispatch( { type: 'reset' } ) }
+      > 0 </button>
+      <button
+        className='btn btn-primary mx-1'
+        onClick={ () => dispatch( { type: 'decrementar' } ) }
+      > -1 </button>
     </>
   )
 }
